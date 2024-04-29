ae19997cea8e5893fb8147b48e46ade66cc2dbc3
Dheeraj <dheerajsehgal0012@gmail.com>
2024-04-29 11:50:37 +0530

    homepage modified

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import LogoTitle from "../../assets/images/logo-s.png";
+import ProfilePic from "../../assets/Profile.png";
 import "./index.scss";
 
 const Home = () => {
@@ -11,6 +12,8 @@ const Home = () => {
   const nameArray = [" ", "D", "h", "e", "e", "r", "a", "j", " "];
   const lastArray = ["e", "h", "g", "a", "l", ","];
   const jobArray = [
+    "A",
+    " ",
     "W",
     "e",
     "b",
@@ -30,14 +33,14 @@ const Home = () => {
   useEffect(() => {
     return setTimeout(() => {
       setLetterClass("text-animate-hover");
-    }, 4000);
+    }, 5000);
   }, []);
 
   return (
     <>
       <div className="container home-page">
         <div className="text-zone">
-          <h1>
+          <h1 className="intro-text">
             <span className={letterClass}>H</span>
             <span className={`${letterClass} _12`}>i,</span>
             <br />
@@ -51,6 +54,7 @@ const Home = () => {
             <img
               src={LogoTitle}
               alt="JavaScript Developer Name, Web Developer Name"
+              className="img-fluid"
             />
             <AnimatedLetters
               letterClass={letterClass}
@@ -70,6 +74,25 @@ const Home = () => {
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
+        </div>
+        <div className="secondary-container">
+          <div className="profile-container">
+            <img src={ProfilePic} alt="profile" />
+          </div>
+          <div className="profile-details">
+            <div className="colz first-col">
+              <span>300+</span>
+              <p>DSA problems solved</p>
+            </div>
+            <div className="colz">
+              <span>18+</span>
+              <p>verified skills</p>
+            </div>
+            <div className="colz third-col">
+              <span>7</span>
+              <p>Projects Built</p>
+            </div>
+          </div>
         </div>
       </div>
 
